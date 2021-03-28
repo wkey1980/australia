4d2ee331499b061116447f5b5d702e1d896ccf7e
will k <28814921+wkey1980@users.noreply.github.com>
2021-03-28 22:18:22 +0100

    update australia theme

--- 404.php
+++ 404.php
@@ -7,3 +7,11 @@
  * @package australia
  */
 ?>
+    <section id="id" class="class">
+
+        <div class="error-404 not-found">
+            <h1><?php esc_html_e( 'No Content Found', 'textDomain' ); ?></h1>
+            <a href="<?php echo esc_url( home_url() ); ?>">Go Home!</a>
+        </div>
+
+    </section>
--- index.php
+++ index.php
@@ -7,3 +7,21 @@
  * @package australia
  */
 ?>
+
+    <?php if ( have_posts() ) : ?>
+
+        <?php while ( have_posts() ) : the_post(); ?>
+
+            <article class="post">
+                <?php the_title(); ?>
+            </article>
+
+        <?php endwhile ?>
+
+    <?php else : ?>
+
+        <article class="post error">
+            <h1 class="404">No posts found</h1>
+        </article>
+
+    <?php endif; ?>
